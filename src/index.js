8597722e344bea355f205ab1a30ff9d751ad70ad
fancn21th <fancn21th@gmail.com>
2024-02-25 10:52:16 +0800

    📦 NEW: js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// <script src="./src/index.js"></script> 执行失败
+// <script type="module" src="./src/index.js></script> 执行成功
+document.querySelector("h2").style.color = "red";
+document.querySelector("h2").textContent = "hello vite";
