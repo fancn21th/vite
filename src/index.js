f7e8907b949d68346fef48120cd35eacad03513a
fancn21th <fancn21th@gmail.com>
2024-02-25 11:42:24 +0800

    📦 NEW: post css 配置 与 tailwind

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./counter.js";
-import styles from "./index.module.css";
+import "./index.css";
 
 // <script src="./src/index.js"></script> 执行失败
 // <script type="module" src="./src/index.js></script> 执行成功
 document.querySelector("h2").textContent = "hello vite";
-document.querySelector("h2").className = styles.title;
