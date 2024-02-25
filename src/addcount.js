2ebda6532c9b39a2334dd0369b6378da60992968
fancn21th <fancn21th@gmail.com>
2024-02-25 11:15:32 +0800

    👌 IMPROVE: 动态加载 css

--- src/addcount.js
+++ src/addcount.js
@@ -1,4 +1,7 @@
+import { count } from "./addcount.module.css";
+
 export const addCount = () => {
   document.querySelector("#counter").textContent =
     parseInt(document.querySelector("#counter").textContent) + 1;
+  document.querySelector("#counter").className = count;
 };
