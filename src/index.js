dbf24536509fbc5782fba2a4e2cc55b8624b833c
fancn21th <fancn21th@gmail.com>
2024-02-25 10:55:37 +0800

    📦 NEW: css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import styles from "./index.module.css";
 // <script src="./src/index.js"></script> 执行失败
 // <script type="module" src="./src/index.js></script> 执行成功
-document.querySelector("h2").style.color = "red";
 document.querySelector("h2").textContent = "hello vite";
+document.querySelector("h2").className = styles.title;
