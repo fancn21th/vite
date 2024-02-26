3738fe1d492873b1aaa03a63783a02df3b08bddc
fancn21th <fancn21th@gmail.com>
2024-02-26 15:28:35 +0800

    📦 NEW:  image with tree shake

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./counter.js";
 import styles from "./index.module.css";
+import salmon from "./salmon.jpg";
 
 // <script src="./src/index.js"></script> 执行失败
 // <script type="module" src="./src/index.js></script> 执行成功
-document.querySelector("h2").textContent = "hello vite";
-document.querySelector("h2").className = styles.title;
+const h2 = document.querySelector("h2");
+h2.textContent = "hello vite";
+h2.className = styles.title;
+
+const image = document.createElement("img");
+image.src = salmon;
+image.className = styles.hero;
+
+h2.parentElement.appendChild(image);
